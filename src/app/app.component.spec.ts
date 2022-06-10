68b36b7f8ddb4bc5ccce19655da0d7510f1edbcf
TheJLifeX <44620382+TheJLifeX@users.noreply.github.com>
2022-06-11 00:21:22 +0200

    Check ngx-scroll-position-restoration compatibility with Angular 10.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular10-project'`, () => {
+  it(`should have as title 'demo-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular10-project');
+    expect(app.title).toEqual('demo-app');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('angular10-project app is running!');
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('.content span')?.textContent).toContain('demo-app app is running!');
   });
 });
